3c07cf587c4fd694706f5cd174d8a8e8bfed0f1c
vik mesilati <vik@arborknot.io>
2022-05-09 18:22:47 +0300

    a

--- HW4/grades.c
+++ HW4/grades.c
@@ -72,7 +72,7 @@ int element_clone_student(void *element, void **output){
     }
     strcpy(new->name,given->name);
     new->id = given->id;
-    new->studGrades = (struct list*)malloc(sizeof(struct list*));
+    new->studGrades = (struct list*)malloc(sizeof(new->studGrades));
     if(!new->studGrades){
         free(new->name);
         free(new);
