b1fc6316383fcdcd261f68e7500a6534cfc6a2f8
Mamat <vik@arborknot.io>
2022-06-07 18:32:15 +0300

    h

--- HW5/firewall.sh
+++ HW5/firewall.sh
@@ -6,12 +6,13 @@ cat parsed-blank.txt | awk '{ gsub(/[ ]+/,""); print }' > parsed-space.txt
 for line in $(cat parsed-space.txt);
 do IFS=',';
 read -a strarr <<< "$line";
+> temp.txt;
 for val in "${strarr[@]}";
 do
-./firewall.exe "$val";
-done > temp.txt
-sort temp.txt | uniq -c | awk '{print $2, $1}';
-sleep 20;
+./firewall.exe "$val" | tee -a temp.txt > /dev/null;
+done
+
+sort temp.txt | uniq -c | awk '{if($1 >= 4) print $2;}';
 done > out.txt
 
 rm parsed.txt parsed-blank.txt parsed-space.txt
